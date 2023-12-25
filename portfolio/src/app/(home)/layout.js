5711ac8f89089429372ba705a5bc835fa28c51cd
Mohammad Hamzah Iqbal <mohammad.iqbal.7@city.ac.uk>
2023-12-25 12:54:04 +0000

    changed analytics to speed insights

--- portfolio/src/app/(home)/layout.js
+++ portfolio/src/app/(home)/layout.js
@@ -1,7 +1,7 @@
 
 import '@/globals.css'
 import { Montserrat } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Header from '../component/headers/Header'
 import Loading from './loading'
 import { Suspense } from 'react'
@@ -27,7 +27,7 @@ export default async function RootLayout({ children }) {
           <Header />
 
           {children}
-          <Analytics />
+          <SpeedInsights />
 
         </Suspense>
       </ body>
